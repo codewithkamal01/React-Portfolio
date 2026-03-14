f16d0d8934a833e8d083581415b65dfdb8910526
Kamal Manna <kamalmanna606@gmail.com>
2026-03-14 12:01:20 +0530

    bugs fixea

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,7 +1,7 @@
-function ProjectCard({ projects, singleButton }) {
+function ProjectCard({ projects, singleButton, livelink, sourcelink }) {
   return (
     <div
-      className={`mt-10 border border-[#AAAAAA] rounded-md w-full px-6 md:px-10 py-6 ${singleButton ? "flex flex-col md:flex-row gap-6" : ""}`}
+      className={`hover:-translate-y-1 hover:shadow-lg transition mt-6 border border-[#AAAAAA] rounded-md w-full px-6 md:px-10 py-6 ${singleButton ? "flex flex-col md:flex-row gap-6" : ""}`}
     >
       <div
         className={`bg-[#E5E5E5] rounded-md px-8 md:px-14 pt-5 ${
@@ -12,7 +12,9 @@ function ProjectCard({ projects, singleButton }) {
       </div>
       <div className="pt-6">
         <h1 className="text-xl md:text-2xl font-semibold ">{projects.title}</h1>
-        <p className="pt-3 text-[#5B5A5A] text-sm md:text-base">{projects.description}</p>
+        <p className="pt-3 text-[#5B5A5A] text-sm md:text-base">
+          {projects.description}
+        </p>
         <div className="flex gap-4 pt-5 flex-wrap">
           {projects.tech.map((item, i) => (
             <span
@@ -26,18 +28,36 @@ function ProjectCard({ projects, singleButton }) {
 
         <div className="flex flex-col sm:flex-row justify-between mt-8 gap-5">
           {!singleButton && (
-            <button className="flex-1 relative cursor-pointer overflow-hidden px-4 py-2 md:px-5 md:py-3 rounded-lg bg-[#0F0F0F] text-white group">
-              <span className="relative z-10">Live Demo</span>
-              <span className="absolute inset-0 bg-[#EB6200] scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"></span>
-            </button>
+            <a
+              href={projects.livelink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex-1"
+            >
+              <button className="w-full relative cursor-pointer overflow-hidden px-4 py-2 md:px-5 md:py-3 rounded-lg bg-[#0F0F0F] text-white group">
+                <span className="relative z-10">Live Demo</span>
+
+                <span className="absolute inset-0 bg-[#EB6200] scale-x-0 origin-left transition-transform duration-300 group-hover:scale-x-100"></span>
+              </button>
+            </a>
           )}
-          <button
-            className={`flex-1 cursor-pointer px-4 py-2 md:px-5 md:py-3  rounded-lg text-white transition ${
-              singleButton ? "bg-black hover:bg-[#EB6200]" : "bg-[#EB6200]"
-            }`}
+
+          <a
+            href={projects.sourcelink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-1"
           >
-            {singleButton ? "Coming Soon" : "Source Code"}
-          </button>
+            <button
+              className={`w-full cursor-pointer px-4 py-2 md:px-5 md:py-3 rounded-lg text-white transition ${
+                singleButton
+                  ? "bg-black hover:bg-[#EB6200]"
+                  : "bg-[#EB6200] hover:bg-[#c75c0a]"
+              }`}
+            >
+              {singleButton ? "Coming Soon" : "Source Code"}
+            </button>
+          </a>
         </div>
       </div>
     </div>
--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -1,23 +1,30 @@
 import ProjectCard from "./ProjectCard";
+import project2 from "../assets/project2.png";
+import project1 from "../assets/pro1.png";
+import project3 from "../assets/project3.png";
 
 function ProjectList() {
   const projects = [
     {
-      image: "src/assets/project2.png",
+      image: project2,
       title: "Weather Dashboard",
       description:
         "An interactive weather dashboard that provides real-time weather data, forecasts, and visualizations using React.js.",
       tech: ["JavaScript", "API Integration", "React.js"],
+      livelink: "https://weather-app-react-iota.vercel.app/",
+      sourcelink: "https://github.com/codewithkamal01/Weather-App---React",
     },
     {
-      image: "src/assets/pro1.png",
+      image: project1,
       title: "Task Management App",
       description:
         "A collaborative task management application with real-time updates, drag-and-drop functionality, and team collaboration features.",
       tech: ["React.js", "Firebase", "Socket.io"],
+      livelink: "https://noteshub-indol.vercel.app/",
+      sourcelink: "https://github.com/codewithkamal01/notes-app",
     },
     {
-      image: "src/assets/project3.png",
+      image: project3,
       title: "E-commerce Website",
       description:
         "A fully responsive e-commerce platform built with React and Node.js, featuring user authentication, payment integration, and admin dashboard.",
