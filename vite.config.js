7b74c5597d65490d5327fc11b5acacbc56a0d830
Kamal Manna <kamalmanna606@gmail.com>
2026-05-08 20:26:55 +0530

    seo added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://kamal-s-portfolio-alpha.vercel.app/",
+    }),
+  ],
+
   server: {
     host: true,
   },
