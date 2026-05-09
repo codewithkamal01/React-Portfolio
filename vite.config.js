0ddd97e1ac7b17b1387f1960307994e5d55b19a7
Kamal Manna <kamalmanna606@gmail.com>
2026-05-09 21:31:17 +0530

    fix errors

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    sitemap({
-      hostname: "https://kamalmanna.vercel.app",
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
 
   server: {
     host: true,
